779bafb0726eb0e8d0f6df61b0399590ec7f382d
dharmeshoraon <dharmeshoraon.09@gmail.com>
2026-02-17 08:53:30 +0530

    Refactor input handling to remove redundant prompts for continuation

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -2,14 +2,8 @@ a=int(input("Enter a Number:"))
 b=input("/ for division/n * for multiplication /n + for addition /n - for substraction/n ^ for power")
 c=int(input("Enter a second number"))
 d=int(input("Do you want to continue? 1 for yes and 0 for no:"))
-if d==1:
-        a=int(input("Enter a Number:"))
-        b=input("/ for division/n * for multiplication /n + for addition /n - for substraction/n ^ for power")
-        c=int(input("Enter a second number"))
 if d==0:
-    print("Goodbye!")
-if b=="/":
-    print("Answer:",a/c)
+    exit()
 if b=="/":
     print("Answer:",a/c)
 elif b=="*":
