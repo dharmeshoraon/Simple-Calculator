8daca11d240bc20189f4e0f7bf05c5f0e5b38932
dharmeshoraon <dharmeshoraon.09@gmail.com>
2026-02-17 15:29:40 +0530

    Improve operator input prompt and restructure continuation logic for clarity

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,19 +1,20 @@
 a=int(input("Enter a Number:"))
-b=input("/ for division/n * for multiplication /n + for addition /n - for substraction/n ^ for power")
+b=input("1.)/ for division\n2.)* for multiplication\n3.)+ for addition\n4.)- for substraction\n5.)^ for power\nEnter the operator you want to use:")
 c=int(input("Enter a second number"))
-d=int(input("Do you want to continue? 1 for yes and 0 for no:"))
-if d==0:
-    exit()
-elif d==1:
-    if b=="/":
-        print("Answer:",a/c)
-    elif b=="*":
-        print("Answer:",a*c)
-    elif b=="+":
-        print("Answer:",a+c)
-    elif b=="-":
-        print("Answer:",a-c)
-    elif b=="^":
-        print("Answer:",a**c)
+d=int(input("Do you want to continue? 1 for \"yes\"\nor press any number to exit:"))
+if d==1:
+    while d==1:
+        if b=="/":
+            print("Answer:",a/c)
+        elif b=="*":
+            print("Answer:",a*c)
+        elif b=="+":
+            print("Answer:",a+c)
+        elif b=="-":
+            print("Answer:",a-c)
+        elif b=="^":
+            print("Answer:",a**c)
+        break
 else:
-    print("Invalid Operator")
+    exit("You have exited the program")
+
