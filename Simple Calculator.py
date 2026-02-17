f722c4257e9fe933d9c5136e91b11e840dbe4ff1
dharmeshoraon <dharmeshoraon.09@gmail.com>
2026-02-17 15:36:56 +0530

    Refactor continuation logic to improve user experience and streamline exit conditions

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -2,19 +2,18 @@ a=int(input("Enter a Number:"))
 b=input("1.)/ for division\n2.)* for multiplication\n3.)+ for addition\n4.)- for substraction\n5.)^ for power\nEnter the operator you want to use:")
 c=int(input("Enter a second number"))
 d=int(input("Do you want to continue? 1 for \"yes\"\nor press any number to exit:"))
-if d==1:
-    while d==1:
-        if b=="/":
+if d==0:
+    exit("You have exited the program.")
+elif d==1:
+    if b=="/":
             print("Answer:",a/c)
-        elif b=="*":
+    elif b=="*":
             print("Answer:",a*c)
-        elif b=="+":
+    elif b=="+":
             print("Answer:",a+c)
-        elif b=="-":
+    elif b=="-":
             print("Answer:",a-c)
-        elif b=="^":
+    elif b=="^":
             print("Answer:",a**c)
-        break
 else:
-    exit("You have exited the program")
-
+    print("Invalid input. Please enter 0 or 1.")
