9dff6cb5cff941e90b89697dcc70fa670bea5c5e
dharmeshoraon <dharmeshoraon.09@gmail.com>
2026-02-17 08:49:45 +0530

    Add continuation prompt and input handling for repeated calculations

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,6 +1,15 @@
 a=int(input("Enter a Number:"))
 b=input("/ for division/n * for multiplication /n + for addition /n - for substraction/n ^ for power")
 c=int(input("Enter a second number"))
+d=int(input("Do you want to continue? 1 for yes and 0 for no:"))
+if d==1:
+        a=int(input("Enter a Number:"))
+        b=input("/ for division/n * for multiplication /n + for addition /n - for substraction/n ^ for power")
+        c=int(input("Enter a second number"))
+if d==0:
+    print("Goodbye!")
+if b=="/":
+    print("Answer:",a/c)
 if b=="/":
     print("Answer:",a/c)
 elif b=="*":
