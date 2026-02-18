2cdf351dd5ffa5c7a4b4ecf387fde1c4c9418e24
dharmeshoraon <dharmeshoraon.09@gmail.com>
2026-02-18 10:26:29 +0530

    Refactor variable names for clarity and consistency in calculation logic

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,19 +1,19 @@
-a=int(input("Enter a Number:"))
-b=input("1.)/ for division\n2.)* for multiplication\n3.)+ for addition\n4.)- for substraction\n5.)^ for power\nEnter the operator you want to use:")
-c=int(input("Enter a second number"))
-d=int(input("Do you want to continue? 1 for \"yes\"\nor press any number to exit:"))
-if d==0:
+num_1=int(input("Enter a Number:"))
+operation=input("1.)/ for division\n2.)* for multiplication\n3.)+ for addition\n4.)- for substraction\n5.)^ for power\nEnter the operator you want to use:")
+num_2=int(input("Enter a second number"))
+flow=int(input("Do you want to continue? 1 for \"yes\"\nor press any number to exit:"))
+if flow==0:
     exit("You have exited the program.")
-elif d==1:
-    if b=="/":
-            print("Answer:",a/c)
-    elif b=="*":
-            print("Answer:",a*c)
-    elif b=="+":
-            print("Answer:",a+c)
-    elif b=="-":
-            print("Answer:",a-c)
-    elif b=="^":
-            print("Answer:",a**c)
+elif flow==1:
+    if operation=="/":
+            print("Answer:",num_1/num_2)
+    elif operation=="*":
+            print("Answer:",num_1*num_2)
+    elif operation=="+":
+            print("Answer:",num_1+num_2)
+    elif operation=="-":
+            print("Answer:",num_1-num_2)
+    elif operation=="^":
+            print("Answer:",num_1**num_2)
 else:
     print("Invalid input. Please enter 0 or 1.")
